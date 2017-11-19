47a3bab0c0dd7c6dbc36c43db417b11b3987cc49
Ehsan Ahmadi <ehsan5323@gmail.com>
2017-11-19 15:13:44 +0330

    commit branch3_3

--- app/src/main/java/com/example/ehsan/testgithub01/MainActivity.java
+++ app/src/main/java/com/example/ehsan/testgithub01/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "ok branch3", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "ok branch3", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "ok branch3", Toast.LENGTH_SHORT).show();
     }
 }
