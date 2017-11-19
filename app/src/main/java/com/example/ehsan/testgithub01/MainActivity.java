e203069a6e13b103b4f8fea361761ae3f1e2c6bf
Ehsan Ahmadi <ehsan5323@gmail.com>
2017-11-19 15:43:01 +0330

    commit master_3

--- app/src/main/java/com/example/ehsan/testgithub01/MainActivity.java
+++ app/src/main/java/com/example/ehsan/testgithub01/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "ok22", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "ok22", Toast.LENGTH_SHORT).show();
     }
 }
